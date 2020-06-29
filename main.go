f8fe7f39edcdc64e8509751e975daacebe389887
MIchael <michael.wenceslaus@gmail.com>
2020-06-29 17:03:21 +0700

    crate main and print function

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	pb "github.com/michaelwp/go-grpc/protofiles"
+)
+
+func main(){
+	p := &pb.Person{
+		Id:1234,
+		Name: "Roger F",
+		Email: "rf@example.com",
+		Phones: []*pb.Person_PhoneNumber {
+			{Number: "555-4321", Type: pb.Person_HOME},
+		},
+	}
+	printRes(p)
+}
+
+func printRes(p *pb.Person) {
+	p1 := &pb.Person{}
+	body, _ := proto.Marshal(p)
+	_ = proto.Unmarshal(body, p)
+	jsonRes, _ := json.Marshal(p)
+
+	fmt.Println("Original struct loaded from proto file: ", p, "\n")
+	fmt.Println("Marshaled proto data: ", body, "\n")
+	fmt.Println("Unmarshaled struct: ", p1)
+	fmt.Println("Json format: ", string(jsonRes))
+}
+
+
